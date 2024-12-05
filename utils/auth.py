f80d75383422b83f7ee02cc42e59e4c0485c0f4d
Daniel Newman <me@dtnewman.com>
2024-12-05 13:25:01 -0500

    remove python-jose dependency and replace with pyJWT

--- utils/auth.py
+++ utils/auth.py
@@ -1,20 +1,20 @@
-from jose import jwt, JWTError
 from fastapi import Depends, HTTPException, status
-from fastapi.security import SecurityScopes, HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from jwt import PyJWT
+from jwt.exceptions import InvalidTokenError
 import requests
-from typing import Optional
 
 from config import settings
 
 
 class UnauthenticatedException(HTTPException):
     def __init__(self):
-        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Requires authentication")
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Requires authentication123")
 
 
 # 👇 new code
 class VerifyToken:
-    """Does all the token verification using jose"""
+    """Does all the token verification using PyJWT"""
 
     def __init__(self):
         self.cognito_issuer = (
@@ -22,6 +22,7 @@ class VerifyToken:
         )
         self.jwks_url = f"{self.cognito_issuer}/.well-known/jwks.json"
         self.cognito_audience = settings.COGNITO_WEB_CLIENT_ID
+        self.jwt = PyJWT()
 
     def get_jwks(self):
         response = requests.get(self.jwks_url)
@@ -32,15 +33,23 @@ class VerifyToken:
             token = token.replace("Bearer ", "").replace("bearer ", "")
             if token == settings.API_KEY:
                 return {"sub": "system"}
-            header = jwt.get_unverified_header(token)
+            
+            header = self.jwt.get_unverified_header(token)
             kid = header["kid"]
             jwks = self.get_jwks()
             key = [k for k in jwks if k["kid"] == kid][0]
-            claims = jwt.decode(
-                token, key, algorithms=["RS256"], audience=self.cognito_audience, issuer=self.cognito_issuer
+            
+            public_key = self.jwt.algorithms.RSAAlgorithm.from_jwk(key)
+            
+            claims = self.jwt.decode(
+                token,
+                key=public_key,
+                algorithms=["RS256"],
+                audience=self.cognito_audience,
+                issuer=self.cognito_issuer
             )
             return claims
-        except JWTError as e:
+        except InvalidTokenError as e:
             print(e)
             raise UnauthenticatedException()
 
